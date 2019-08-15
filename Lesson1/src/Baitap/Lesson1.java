4f5b4a68bf120b85b3f3f29a76a854c06fe73c20
Atom <50996589+atom0511@users.noreply.github.com>
2019-08-15 23:15:35 +0700

    Update Lesson1.java

--- Lesson1/src/Baitap/Lesson1.java
+++ Lesson1/src/Baitap/Lesson1.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Baitap;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
 public class Lesson1 {
     
     public static void main(String[] args) {
