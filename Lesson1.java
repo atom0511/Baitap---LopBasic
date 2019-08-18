3def73f18ae227626b368dfbff772d14fac2294b
Atom <50996589+atom0511@users.noreply.github.com>
2019-08-18 12:08:32 +0700

    Add files via upload

---
+++ Lesson1.java
@@ -0,0 +1,90 @@
+package Baitap;
+
+import java.util.Scanner;
+
+public class Lesson1 {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        //Bai1
+        
+        System.out.println("BAI 1");
+    
+        System.out.println("");
+        System.out.print("Nhap vao gia tri a = ");
+    double a = sc.nextInt();
+        System.out.print("Nhap vao gia tri b = ");
+    double b = sc.nextInt();
+        System.out.println("Tong a + b = " + (a+b) );
+        System.out.println("Hieu a - b = " + (a-b) );
+        System.out.println("Tich a * b = " + (a*b) );
+        if(b==0){
+            System.out.println("Thuong a / b = Math Error");
+        } else { 
+        System.out.println("Thuong a / b = " + (a/b) );
+        }
+        System.out.println("Luy thua a ^ b = " + Math.pow(a, b) );
+       
+    
+        //Bai2
+        
+        System.out.println("------------------------");
+        System.out.println("BAI 2");
+        System.out.println("");
+        
+        System.out.println("Phuong trinh bac 2 co dang: ax^2 + bx + c = 0");
+        System.out.println("Nhap lan luot cac he so a, b, c");
+        
+        System.out.print("a = ");
+        double x = sc.nextInt();
+        System.out.print("b = ");
+        double y = sc.nextInt();
+        System.out.print("c = ");
+        double z = sc.nextInt();
+        
+        if( x==0 ){
+            System.out.println("Dieu kien ptb2 a khac 0");
+        } else{     
+       
+        System.out.println("Phuong trinh cua ban la: " + x + "x^2 + " + y + "x + " + z + " = 0");
+        
+        double delta = y*y - 4*x*z;
+        double canDelta = Math.sqrt(delta);
+        double nghiemKep = (-y) / (2*x);
+        double nghiem1 = (-y + canDelta) / (2*x);
+        double nghiem2 = (-y - canDelta) / (2*x);
+        System.out.println("Co delta ▲ = b^2 - 4a*c = " + delta);
+        
+        if(delta < 0){
+            System.out.println("Phuong trinh vo nghiem");
+        } else if (delta == 0){
+            System.out.println("Phuong trinh co nghiem kep: x1 = x2 = " + nghiemKep );
+        } else {
+            System.out.println("Phuong trinh co 2 nghiem:\n x1 = " + nghiem1 + "x2 = \n" + nghiem2 );
+             }
+         }
+        
+        //Bai3
+        
+        System.out.println("----------------------");
+        System.out.println("BAI 3");
+        System.out.println("");
+        
+        System.out.print("Nhap vao 1 so nguyen bat ki m = ");
+        int m = sc.nextInt();
+        System.out.println("Cac so nguyen to nho hon m la ");
+        for(int i = m ; i > 0 ; i -- ){
+            double n = Math.sqrt(i);
+            int k = 0;
+            for ( int  j = 1 ; j <= n; j++){
+                 if(i % j == 0){
+                     k++;
+                     }
+            }
+            if (k == 1 && i != m && i != 1) System.out.println("i = " + i);
+    }
+        
+    }
+}
