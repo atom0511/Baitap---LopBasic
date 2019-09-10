a0e4d541aecf0c70da33828ed7b36e0c6fd4d73e
Atom <50996589+atom0511@users.noreply.github.com>
2019-09-10 18:14:11 +0700

    Add files via upload

---
+++ Lesson8_2.java
@@ -0,0 +1,34 @@
+package Baitap;
+
+import java.util.Scanner;
+
+public class Lesson8_2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of array: ");
+        int leng = sc.nextInt();
+        int[] arr = new int[leng];
+        System.out.println("Enter the element of array: ");
+        for (int i = 0; i < leng; i++) {
+            String token = sc.next();
+            arr[i] = Integer.parseInt(token);
+        }
+        for (int i = 0; i < leng; i++) {
+            if (arr[i] < 10 && arr[i] % 2 == 1) {
+                System.out.println("The element to find is: " + arr[i]);
+                break;
+            } else {
+                int num1 = arr[i];
+                while (true) {
+                    if (num1 > 1) {
+                        num1 = num1 / 10;
+                    }
+                    System.out.println("The element to find is: " + "arr[" + i + "] = " + arr[i]);
+                    break;
+                }
+                break;
+            }
+        }
+    }
+}
