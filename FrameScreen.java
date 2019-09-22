78f6ccd34299f8c37d702f50f051ce2f4826881c
Atom <50996589+atom0511@users.noreply.github.com>
2019-09-22 18:32:52 +0700

    Update FrameScreen.java

--- FrameScreen.java
+++ FrameScreen.java
@@ -5,50 +5,60 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import static laplanh.GameScreen.countEnd;
 
-public class FrameScreen extends JFrame {
+public class FrameScreen extends JFrame  {
 
     GameScreen gameScreen;
     public static final int row = 4;
     public static final int column = 4;
     public static int elements = row * column / 2 + 1;
     public static Icon[][] arr = new Icon[row][column];
-    public static Icon[] arr1 = new Icon[column / 2];
+    public static Icon[] arr1 = imageRandom();
     public static Icon hoicham = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\hoicham.jpg");
 
     public FrameScreen() {
         gameScreen = new GameScreen();
         add(gameScreen);
         creatAndShow();
-
+        
     }
 
-    public void imageRandom() {
-        arr1[0] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\image\\dog1.jpg");
-        arr1[1] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\image\\dog1.jpg");
-        arr1[2] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\image\\dog1.jpg");
-        arr1[3] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\image\\dog1.jpg");
-        arr1[4] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\image\\dog1.jpg");
-        arr1[5] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\image\\dog1.jpg");
-        arr1[6] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\image\\dog1.jpg");
-        arr1[7] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\image\\dog1.jpg");
+    public static Icon[] imageRandom() {
+        Icon[] arrIM = new Icon[8];
+        arrIM[0] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\dog1.jpg");
+        arrIM[1] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\dog2.jpg");
+        arrIM[2] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\dog3.jpg");
+        arrIM[3] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\dog4.jpg");
+        arrIM[4] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\dog5.jpg");
+        arrIM[5] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\dog6.jpg");
+        arrIM[6] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\dog7.jpg");
+        arrIM[7] = new ImageIcon("C:\\Users\\Admin\\Documents\\NetBeansProjects\\LapLanh\\image\\dog8.jpg");
+        return arrIM;
     }
 
     static Icon[][] arrayRandom() {
         Icon[][] arrRd = new Icon[row][column];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arrRd[i][j] = hoicham;
+            }
+        }
         Random rd = new Random();
         int num = 1;
-        for (int i = 0; i < 8; i++) {
+        
+        boolean flag = true;
+        while(flag){
             int m = rd.nextInt(row);
             int n = rd.nextInt(column);
             if (arrRd[m][n] == hoicham) {
-                arrRd[m][n] = arr1[i];
+                arrRd[m][n] = arr1[num-1];
                 num++;
+               
                 if (num == elements) {
                     num = 1;
                 }
             }
-
             int count = 0;
             for (int x = 0; x < row; x++) {
                 for (int y = 0; y < column; y++) {
@@ -57,12 +67,10 @@ public class FrameScreen extends JFrame {
                     }
                 }
             }
-
             if (count == 0) {
-                break;
+                flag = false;
             }
         }
-
         return arrRd;
     }
 
@@ -77,6 +85,10 @@ public class FrameScreen extends JFrame {
     public static void main(String[] args) {
         new FrameScreen();
         arr = arrayRandom();
+        arr1 = imageRandom();
+        Sound sound = new Sound();
+       
+        
     }
 
 }
